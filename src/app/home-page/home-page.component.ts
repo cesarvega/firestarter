1f83ba4a1f334d24ea2195a0c3e1abafb8072d9c
cesarvega <cesarvega.col@gmail.com>
2020-02-10 10:13:57 -0500

    angular animtations successfully installed

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -13,23 +13,90 @@ export class HomePageComponent implements OnInit, OnDestroy {
 
   products: Product[];
   productSub: Subscription;
-  HEROES = [
-    { id: 1, name: 'Superman' },
-    { id: 2, name: 'Batman' },
-    { id: 5, name: 'BatGirl' },
-    { id: 3, name: 'Robin' },
-    { id: 3, name: 'Robin' },
-    { id: 3, name: 'Robin' },
-    { id: 3, name: 'Robin' },
-    { id: 3, name: 'Robin' },
-    { id: 3, name: 'Robin' },
-    { id: 3, name: 'Robin' },
-    { id: 3, name: 'Robin' },
-    { id: 4, name: 'Flash' }
+  playMusicOnce = true;
+  animations = [
+    { animationName: "bounce" },
+    { animationName: "flash" },
+    { animationName: "pulse" },
+    { animationName: "rubberBand" },
+    { animationName: "shake" },
+    { animationName: "swing" },
+    { animationName: "tada" },
+    { animationName: "wobble" },
+    { animationName: "jello" },
+    { animationName: "heartBeat" },
+    { animationName: "bounceIn" },
+    { animationName: "bounceInDown" },
+    { animationName: "bounceInLeft" },
+    { animationName: "bounceInRight" },
+    { animationName: "bounceInUp" },
+    { animationName: "bounceOut" },
+    { animationName: "bounceOutDown" },
+    { animationName: "bounceOutLeft" },
+    { animationName: "bounceOutRight" },
+    { animationName: "bounceOutUp" },
+    { animationName: "fadeIn" },
+    { animationName: "fadeInDown" },
+    { animationName: "fadeInDownBig" },
+    { animationName: "fadeInLeft" },
+    { animationName: "fadeInLeftBig" },
+    { animationName: "fadeInRight" },
+    { animationName: "fadeInRightBig" },
+    { animationName: "fadeInUp" },
+    { animationName: "fadeInUpBig" },
+    { animationName: "fadeOut" },
+    { animationName: "fadeOutDown" },
+    { animationName: "fadeOutDownBig" },
+    { animationName: "fadeOutLeft" },
+    { animationName: "fadeOutLeftBig" },
+    { animationName: "fadeOutRight" },
+    { animationName: "fadeOutRightBig" },
+    { animationName: "fadeOutUp" },
+    { animationName: "fadeOutUpBig" },
+    { animationName: "flip" },
+    { animationName: "flipInX" },
+    { animationName: "flipInY" },
+    { animationName: "flipOutX" },
+    { animationName: "flipOutY" },
+    { animationName: "lightSpeedIn" },
+    { animationName: "lightSpeedOut" },
+    { animationName: "rotateIn" },
+    { animationName: "rotateInDownLeft" },
+    { animationName: "rotateInDownRight" },
+    { animationName: "rotateInUpLeft" },
+    { animationName: "rotateInUpRight" },
+    { animationName: "rotateOut" },
+    { animationName: "rotateOutDownLeft" },
+    { animationName: "rotateOutDownRight" },
+    { animationName: "rotateOutUpLeft" },
+    { animationName: "rotateOutUpRight" },
+    { animationName: "slideInUp" },
+    { animationName: "slideInDown" },
+    { animationName: "slideInLeft" },
+    { animationName: "slideInRight" },
+    { animationName: "slideOutUp" },
+    { animationName: "slideOutDown" },
+    { animationName: "slideOutLeft" },
+    { animationName: "slideOutRight" },
+    { animationName: "zoomIn" },
+    { animationName: "zoomInDown" },
+    { animationName: "zoomInLeft" },
+    { animationName: "zoomInRight" },
+    { animationName: "zoomInUp" },
+    { animationName: "zoomOut" },
+    { animationName: "zoomOutDown" },
+    { animationName: "zoomOutLeft" },
+    { animationName: "zoomOutRight" },
+    { animationName: "zoomOutUp" },
+    { animationName: "hinge" },
+    { animationName: "jackInTheBox" },
+    { animationName: "rollIn" },
+    { animationName: "rollOut" },
   ];
   panelOpenState = false;
   isPopVisible = false;
   whisMe = false;
+  whisMeFilled = false;
   badgeNumber = 0;
   images: { "id": number; "url": string; }[];
   constructor(public boardService: BoardService) { }
@@ -52,6 +119,21 @@ export class HomePageComponent implements OnInit, OnDestroy {
   }
 
 
+  playAmbientMusic() {
+    if (this.playMusicOnce) {
+      setTimeout(() => {
+        let audio = new Audio();
+        audio.src = "assets/material_product_sounds/mrvr/ambiemceVrMusic_v01.wav";
+        audio.load();
+        audio.play();
+        this.playMusicOnce = false;
+      }, 1000);
+    }
+
+
+  }
+
+
   ngOnDestroy(): void {
     this.productSub.unsubscribe();
   }
@@ -62,37 +144,58 @@ export class HomePageComponent implements OnInit, OnDestroy {
     let audio = new Audio();
     switch (butonName) {
 
-      case 'pop':
+      case 'heart':
+        // audio.src = "assets/material_product_sounds/wav/03 Primary System Sounds/state-change_confirm-up.wav";
+        audio.src = "assets/material_product_sounds/mrvr/pufff.wav";
+        this.whisMe = !this.whisMe;
         this.isPopVisible = !this.isPopVisible;
+
+        setTimeout(() => {
+          this.isPopVisible = !this.isPopVisible;
+          this.whisMeFilled = !this.whisMeFilled;
+          setTimeout(() => {
+
+          }, 1000);
+        }, 900);
+
+        audio.load();
+        audio.play();
         break;
 
-      case 'heart':
-        audio.src = "assets/material_product_sounds/wav/03 Primary System Sounds/state-change_confirm-up.wav";
-        this.isPopVisible = !this.isPopVisible;
-        this.whisMe = !this.whisMe;
+      case 'song':
+        audio.src = "assets/material_product_sounds/mrvr/pufff.wav";
+        audio.load();
+        audio.play();
         break;
 
       case 'remove':
         audio.src = "assets/material_product_sounds/wav/01 Hero Sounds/hero_simple-celebration-03.wav";
         this.badgeNumber = this.badgeNumber - 1;
+        audio.load();
+        audio.play();
         break;
 
       case 'price':
         audio.src = "assets/material_product_sounds/wav/04 Secondary System Sounds/alert_error-02.wav";
+        audio.load();
+        audio.play();
         break;
 
       case 'cigar':
         audio.src = "assets/material_product_sounds/wav/04 Secondary System Sounds/alert_error-02.wav";
+        audio.load();
+        audio.play();
         break;
 
       default:
         audio.src = "assets/material_product_sounds/wav/01 Hero Sounds/hero_simple-celebration-01.wav";
         this.badgeNumber = this.badgeNumber + 1;
+        audio.load();
+        audio.play();
         break;
     }
 
-    audio.load();
-    audio.play();
+
   }
 
 }
